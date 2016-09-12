2e4abaa6cabd635bd30526d1dbc8d3bcee2d8963
Nikhil Kumar Singh <nikhilksingh97@gmail.com>
2016-09-12 03:31:36 -0700

    Add new functions

--- user_details.py
+++ user_details.py
@@ -8,24 +8,39 @@ graph = facebook.GraphAPI(access_token=access_token, version='2.5')
 
 
 
-def get_user_posts():
-   myposts=[]
 
-   #get my posts on page 1
-   posts = graph.get_connections('me',connection_name='posts')
-   myposts = posts['data']
-
+def get_all_pages(posts):
+   allposts=[]
+   allposts = posts['data']
    while(1):
       try:
          next_page_url=posts['paging']['next']  #get url for next page
       except KeyError:
          break
       posts = requests.get(next_page_url).json()
-      myposts += posts['data']
+      allposts += posts['data']
+   return allposts
+   
 
+
+def get_post_likes(post):
+   mylikes=[]
+   try:
+     likes=graph.get_connections(post['id'],"likes")
+     mylikes = get_all_pages(likes)
+   except:
+     pass 
+   return mylikes
+
+
+def get_user_posts():
+   myposts=[]
+
+   #get my posts on page 1
+   posts = graph.get_connections('me',connection_name='posts')
+   #visit all pages
+   myposts = get_all_pages(posts)
    return myposts
-
-
 
 
 
