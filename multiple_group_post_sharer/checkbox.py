cc2b2fd33cc665a54b8cafa1d64f46329eb6f05d
Nikhil Kumar Singh <nikhilksingh97@gmail.com>
2016-09-17 00:45:59 -0700

    scrollable now!

--- multiple_group_post_sharer/checkbox.py
+++ multiple_group_post_sharer/checkbox.py
@@ -6,11 +6,28 @@ class CHECKBOX():
         self.root = tk.Tk()
         self.cb = []     # check_buttons
         self.cb_v = []   # check_button_values
-        self.checked = [] 
-        for ix,text in enumerate(mylist):
+        self.checked = []
+
+        self.frame = tk.Frame(self.root,width=300,height=300)
+        self.frame.grid(row = 0,column = 0)
+        
+        self.vsb = tk.Scrollbar(self.frame, orient="vertical")
+        self.text = tk.Text(self.frame, width=40, height=20,bg="light blue", 
+                            yscrollcommand=self.vsb.set)
+        self.vsb.config(command=self.text.yview)
+        self.vsb.pack(side="right", fill="y")
+        
+
+
+        for ix,btext in enumerate(mylist):
             self.cb_v.append(tk.IntVar())
-            self.cb.append(tk.Checkbutton(self.root, text=text, variable=self.cb_v[ix], command=self.cb_checked))
+            self.cb.append(tk.Checkbutton(self.frame, text=btext, variable=self.cb_v[ix], command=self.cb_checked, bg = "light blue"))
             self.cb[ix].grid(row=ix, column=0, sticky='w')
+            self.text.window_create("end", window=self.cb[ix])
+            self.text.insert("end", "\n")
+
+        self.text.pack(side="left", fill="both", expand=True)
+        
 
         self.label = tk.Label(self.root, width=50,bg = 'light green')
         self.label.grid(row=ix+1, column=0, sticky='w')
@@ -18,6 +35,7 @@ class CHECKBOX():
         self.button_label.grid(row=ix+2, column=0, sticky='s')
         self.button = tk.Button(self.button_label, text='Done',command = self.result)
         self.button.pack()
+
         self.root.mainloop()
 
 
